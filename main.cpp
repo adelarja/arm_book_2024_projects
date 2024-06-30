66eef77fe52c3876a263e79acea1ad97b95d58f3
Adel Arja <adel.arja@mi.unc.edu.ar>
2024-06-30 23:02:30 +0000

    Added manual mode management.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,31 @@
 
 #include "mbed.h"
 #include "arm_book_lib.h"
+#include <cstdint>
 
 #define WATER_LEVEL_THRESHOLD 0.8
 #define NUMBER_OF_AVG_SAMPLES 100
+#define DEBOUNCE_TIME 30000
+#define MAIN_DELAY 10
+#define MANUAL_MODE_BUTTON 0
+#define SOLENOID_VALVE_1_BUTTON 1
+#define SOLENOID_VALVE_2_BUTTON 2
+#define WATER_PUMP_BUTTON 3
+
+typedef struct {
+    DigitalIn pin;
+    int state;
+    int lastState;
+    Timer debounceTimer;
+    bool pressed;
+} Button;
+
+Button buttons[4] = {
+    {DigitalIn(D5), 1, 1, Timer(), false},
+    {DigitalIn(D6), 1, 1, Timer(), false},
+    {DigitalIn(D7), 1, 1, Timer(), false},
+    {DigitalIn(D8), 1, 1, Timer(), false},
+};
 
 DigitalOut solenoidValve1(D2);
 DigitalOut solenoidValve2(D3);
@@ -38,20 +60,31 @@ float levelSensor3ReadingsSum = 0.0;
 float levelSensor3ReadingsArray[NUMBER_OF_AVG_SAMPLES];
 float tank3Level = 0.0;
 
+bool manualMode = false;
+
 void inputsInit();
 void outputsInit();
 void getTanksWaterLevel();
 void manageWaterLevel();
 void availableCommands();
+void debounceButton(Button*);
+void buttonsInit();
 void uartTask();
+void manualLevelManagement();
+void checkManualManagementSwitch();
 
 int main()
 {
     inputsInit();
     outputsInit();
+    buttonsInit();
     while(true) {
-        getTanksWaterLevel();
-        manageWaterLevel();
+        if(manualMode == true) {
+            manualLevelManagement();
+        } else {
+            getTanksWaterLevel();
+            manageWaterLevel();
+        }
         uartTask();
     }
 }
@@ -171,4 +204,57 @@ void availableCommands()
     uartUsb.write( "Available commands:\r\n", 21 );
     uartUsb.write( "Press 'T' to get the water level in all tanks.\r\n", 48 );
     uartUsb.write( "Press 'S' to get the solenoid valves and pump system state.\r\n", 61 );
+}
+
+void debounceButton(Button *button) {
+    uint8_t reading = button->pin.read();
+
+    if(reading != button->lastState) {
+        button->debounceTimer.reset();
+    }
+
+    if(button->debounceTimer.elapsed_time().count() >= DEBOUNCE_TIME) {
+        if(reading != button->state) {
+            button->state = reading;
+        }
+
+        if(button->state == LOW) {
+            button->pressed = true;
+        } else {
+            button->pressed = false;   
+        }
+    }
+    button->lastState = reading;
+}
+
+void buttonsInit() {
+    for (int i = 0; i < 4; i++) {
+        buttons[i].debounceTimer.start();
+        buttons[i].pin.mode(PullUp);
+    }
+}
+
+void manualLevelManagement() {
+    for(int i = 0; i < 4; i++) {
+        debounceButton(&buttons[i]);
+    }
+
+    if(buttons[MANUAL_MODE_BUTTON].pressed == true) {
+        manualMode = !manualMode;
+    } else if(buttons[SOLENOID_VALVE_1_BUTTON].pressed == true) {
+        solenoidValve1 == 1 ? solenoidValve1 = 0 : solenoidValve1 = 1;
+        ld1 == 1 ? ld1 = 0: ld1 = 1;
+    } else if(buttons[SOLENOID_VALVE_2_BUTTON].pressed == true) {
+        solenoidValve2 == 1 ? solenoidValve2 = 0 : solenoidValve2 = 1;
+        ld2 == 1 ? ld2 = 0: ld2 = 1;
+    } else if(buttons[WATER_PUMP_BUTTON].pressed == true) {
+        waterPump == 1 ? waterPump = 0 : waterPump = 1;
+        ld3 == 1 ? ld3 = 0: ld3 = 1;
+    }
+}
+
+void checkManualManagementSwitch() {
+    debounceButton(&buttons[MANUAL_MODE_BUTTON]);
+    if(buttons[MANUAL_MODE_BUTTON].pressed == true)
+        manualMode = !manualMode;
 }
